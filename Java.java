2294a85bfe13c5b6d3884b40d1f39d4539d92119
Riya <soniriya2409@gmail.com>
2025-01-07 16:30:03 +0530

    jhyugd

--- Java.java
+++ Java.java
@@ -1,12 +1,11 @@
+import java.util.*;
 class Java{
 public static void main(String args[]){
 Scanner sc = new Scanner(System.in);
 int a = sc.nextInt();
 int b = sc.nextInt();
-int add = 0;
-add = a+b;
-System.out.println("Addition is "+add);
-
+mul = a*b;
+System.out.println("Multiplication "+mul);
 
 }
 }
