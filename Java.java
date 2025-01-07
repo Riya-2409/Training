5255ec9ce16f773a389dd8023c6ee9536083e57c
Riya <soniriya2409@gmail.com>
2025-01-07 14:04:26 +0530

    added

--- Java.java
+++ Java.java
@@ -1,5 +1,12 @@
 class Java{
 public static void main(String args[]){
-System.out.println("HelloWorld");
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+int add = 0;
+add = a+b;
+System.out.println("Addition is "+add);
+
+
 }
 }
